9094708b9b967ad52940fc94f3aed265159999f1
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-23 15:22:31 +0100

    Sort messages by newest

--- routes/index.ts
+++ routes/index.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   // currently very inefficient, needs to implement loading and paging
   // however, that is outside of the scope of this project
-  const messages = await Message.find();
+  const messages = await Message.find().sort({ added: -1 });
   res.render('index', { title: 'Mini Message Board', messages } as IndexLocals);
 });
 
