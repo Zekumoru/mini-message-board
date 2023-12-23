37c70bb8baaf13d555959adf2e3d4f74f9609041
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-12-23 17:09:21 +0100

    Create error page

--- app.ts
+++ app.ts
@@ -50,11 +50,12 @@ app.use(
     next: NextFunction,
   ) => {
     // set locals, only providing error in development
-    res.locals.message = err.message;
+    const statusCode = err.status || 500;
+    res.locals.title = `${statusCode} ${err.message}`;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
+    res.status(statusCode);
     res.render('error');
   },
 );
